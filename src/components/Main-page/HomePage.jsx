e3a005e045c2c13a15d8c99f7a54fd6440789ff8
Chirag <chiragwork2403@gmail.com>
2024-01-16 22:34:00 +0530

    removed the our team h1 tag

--- src/components/Main-page/HomePage.jsx
+++ src/components/Main-page/HomePage.jsx
@@ -262,8 +262,8 @@ export default function HomePage(props) {
         </Row> */}
         {/*TimeLine Section*/}
         <div>
-          <h1 className='myCustomFontSize'>Hackathon Timeline</h1>
-          </div>
+          <h1 className="myCustomFontSize">Hackathon Timeline</h1>
+        </div>
         <Row className="timeline justify-content-center" id="timeline">
           <TimeLine />
         </Row>
